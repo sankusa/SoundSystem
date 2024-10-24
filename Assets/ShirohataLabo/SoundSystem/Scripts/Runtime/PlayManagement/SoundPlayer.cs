2c9f4a4e99728729666991b7e9da6feffd151014
sankusa <91744435+sankusa@users.noreply.github.com>
2024-10-24 14:14:42 +0900

    SoundPlayerに各種音声加工機能を追加

--- Assets/ShirohataLabo/SoundSystem/Scripts/Runtime/PlayManagement/SoundPlayer.cs
+++ Assets/ShirohataLabo/SoundSystem/Scripts/Runtime/PlayManagement/SoundPlayer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace SoundSystem {
     public partial class SoundPlayer {
+        readonly GameObject _gameObject;
         readonly Transform _transform;
         readonly AudioSource _audioSource;
         public AudioClip Clip => _audioSource.clip;
@@ -18,6 +19,21 @@ namespace SoundSystem {
         }
         public float Time => _audioSource.clip == null ? 0 : (float)_audioSource.timeSamples / _audioSource.clip.frequency;
         public bool Loop => _audioSource.loop;
+
+        public bool BypassEffects => _audioSource.bypassEffects;
+        public bool BypassListenerEffects => _audioSource.bypassListenerEffects;
+        public bool BypassReverbZones => _audioSource.bypassReverbZones;
+        public bool IgnoreListenerPause => _audioSource.ignoreListenerPause;
+        public bool IgnoreListenerVolume => _audioSource.ignoreListenerVolume;
+        public float StereoPan => _audioSource.panStereo;
+        public float SpatialBlend => _audioSource.spatialBlend;
+        public float ReverbZoneMix => _audioSource.reverbZoneMix;
+        public float DopplerLevel => _audioSource.dopplerLevel;
+        public float Spread => _audioSource.spread;
+        public AudioRolloffMode VolumeRollof => _audioSource.rolloffMode;
+        public float MinDistance => _audioSource.minDistance;
+        public float MaxDistance => _audioSource.maxDistance;
+
 
         // ・AudioSource.isPlayingはポーズ時にfalseになってしまう
         // ・AudioSourceは再生位置が終端に達した場合、SoundPlayerが検知する前に自律的に状態をリセットしてしまう場合がある
@@ -42,11 +58,18 @@ namespace SoundSystem {
 
         Transform _spawnPoint;
 
+        public UnityEngine.AudioHighPassFilter AudioHighPassFilter { get; private set; }
+        public UnityEngine.AudioLowPassFilter AudioLowPassFilter { get; private set; }
+        public UnityEngine.AudioEchoFilter AudioEchoFilter { get; private set; }
+        public UnityEngine.AudioDistortionFilter AudioDistortionFilter { get; private set; }
+        public UnityEngine.AudioReverbFilter AudioReverbFilter { get; private set; }
+        public UnityEngine.AudioChorusFilter AudioChorusFilter { get; private set; }
+
         public SoundPlayer(GameObject parentObject, SoundPlayerGroupSetting groupSetting, List<Volume> volumes) {
-            GameObject gameObject = new("Player");
-            _transform = gameObject.transform;
+            _gameObject = new("Player");
+            _transform = _gameObject.transform;
             _transform.SetParent(parentObject.transform);
-            AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+            AudioSource audioSource = _gameObject.AddComponent<AudioSource>();
             _audioSource = audioSource;
 
             _audioSource.outputAudioMixerGroup = groupSetting.OutputAudioMixerGroup;
@@ -132,8 +155,131 @@ namespace SoundSystem {
             return this;
         }
 
-        public void SetTime(float time) {
+        public SoundPlayer SetTime(float time) {
             _audioSource.timeSamples = _audioSource.clip == null ? 0 : (int)(time * _audioSource.clip.frequency);
+            return this;
+        }
+
+        public SoundPlayer SetBypassEffects(bool bypassEffects) {
+            _audioSource.bypassEffects = bypassEffects;
+            return this;
+        }
+
+        public SoundPlayer SetBypassListenerEffects(bool bypassListenerEffects) {
+            _audioSource.bypassListenerEffects = bypassListenerEffects;
+            return this;
+        }
+
+        public SoundPlayer SetBypassReverbZones(bool bypassReverbZones) {
+            _audioSource.bypassReverbZones = bypassReverbZones;
+            return this;
+        }
+
+        public SoundPlayer SetIgnoreListenerPause(bool ignoreListenerPause) {
+            _audioSource.ignoreListenerPause = ignoreListenerPause;
+            return this;
+        }
+
+        public SoundPlayer SetIgnoreListenerVolume(bool ignoreListenerVolume) {
+            _audioSource.ignoreListenerVolume = ignoreListenerVolume;
+            return this;
+        }
+
+        public SoundPlayer SetStereoPan(float stereoPan) {
+            _audioSource.panStereo = stereoPan;
+            return this;
+        }
+
+        public SoundPlayer SetSpatialBlend(float spatialBlend) {
+            _audioSource.spatialBlend = spatialBlend;
+            return this;
+        }
+
+        public SoundPlayer SetReverbZoneMix(float reverbZoneMix) {
+            _audioSource.reverbZoneMix = reverbZoneMix;
+            return this;
+        }
+
+        public SoundPlayer SetDopplerLevel(float dopplerLevel) {
+            _audioSource.dopplerLevel = dopplerLevel;
+            return this;
+        }
+
+        public SoundPlayer SetSpread(float spread) {
+            _audioSource.spread = spread;
+            return this;
+        }
+
+        public SoundPlayer SetVolumeRollof(AudioRolloffMode rolloffMode) {
+            _audioSource.rolloffMode = rolloffMode;
+            return this;
+        }
+
+        public SoundPlayer SetMinDistance(float minDistance) {
+            _audioSource.minDistance = minDistance;
+            return this;
+        }
+
+        public SoundPlayer SetMaxDistance(float maxDistance) {
+            _audioSource.maxDistance = maxDistance;
+            return this;
+        }
+
+        public AnimationCurve GetCustomCurve(AudioSourceCurveType type) {
+            return _audioSource.GetCustomCurve(type);
+        }
+
+        public SoundPlayer SetCustomCurve(AudioSourceCurveType type, AnimationCurve curve) {
+            _audioSource.SetCustomCurve(type, curve);
+            return this;
+        }
+
+        public SoundPlayer EnableAudioHighPassFilter() {
+            if (AudioHighPassFilter == null) {
+                AudioHighPassFilter = _gameObject.AddComponent<UnityEngine.AudioHighPassFilter>();
+            }
+            AudioHighPassFilter.enabled = true;
+            return this;
+        }
+
+        public SoundPlayer EnableAudioLowPassFilter() {
+            if (AudioLowPassFilter == null) {
+                AudioLowPassFilter = _gameObject.AddComponent<UnityEngine.AudioLowPassFilter>();
+            }
+            AudioLowPassFilter.enabled = true;
+            return this;
+        }
+
+        public SoundPlayer EnableAudioEchoFilter() {
+            if (AudioEchoFilter == null) {
+                AudioEchoFilter = _gameObject.AddComponent<UnityEngine.AudioEchoFilter>();
+            }
+            AudioEchoFilter.enabled = true;
+            return this;
+        }
+
+        public SoundPlayer EnableAudioDistortionFilter() {
+            if (AudioDistortionFilter == null) {
+                AudioDistortionFilter = _gameObject.AddComponent<UnityEngine.AudioDistortionFilter>();
+            }
+            AudioDistortionFilter.enabled = true;
+            return this;
+        }
+
+        public SoundPlayer EnableAudioReverbFilter() {
+            if (AudioReverbFilter == null) {
+                AudioReverbFilter = _gameObject.AddComponent<UnityEngine.AudioReverbFilter>();
+            }
+            AudioReverbFilter.enabled = true;
+            return this;
+        }
+
+        public SoundPlayer EnableAudioChorusFilter() {
+            if (AudioChorusFilter == null) {
+                AudioChorusFilter = _gameObject.AddComponent<UnityEngine.AudioChorusFilter>();
+            }
+            AudioChorusFilter.enabled = true;
+            return this;
         }
 
         public SoundPlayer Play() {
@@ -193,6 +339,28 @@ namespace SoundSystem {
             _audioSource.pitch = 1;
             _audioSource.loop = _groupSetting.DefaultLoop;
             _audioSource.timeSamples = 0;
+
+            _audioSource.bypassEffects = false;
+            _audioSource.bypassListenerEffects = false;
+            _audioSource.bypassReverbZones = false;
+            _audioSource.ignoreListenerPause = false;
+            _audioSource.ignoreListenerVolume = false;
+            _audioSource.panStereo = 0;
+            _audioSource.spatialBlend = 0;
+            _audioSource.reverbZoneMix = 1;
+            _audioSource.dopplerLevel = 1;
+            _audioSource.spread = 0;
+            _audioSource.rolloffMode = AudioRolloffMode.Logarithmic;
+            _audioSource.minDistance = 1;
+            _audioSource.maxDistance = 500;
+
+            // Filters
+            AudioHighPassFilter.DisableAndReset();
+            AudioLowPassFilter.DisableAndReset();
+            AudioEchoFilter.DisableAndReset();
+            AudioDistortionFilter.DisableAndReset();
+            AudioReverbFilter.DisableAndReset();
+            AudioChorusFilter.DisableAndReset();
         }
 
         public void Update(float deltaTime) {
